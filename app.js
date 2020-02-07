61fc7c8a725d1930bd3b03167c5fc211c35d8559
Jethet <henriette.hettinga@gmail.com>
2020-02-07 12:08:39 +0100

    Specify GET requests, add html with form

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-//SET TEMPLATE ENGINE
-app.set('view engine', 'hbs');
+// SET THE TEMPLATE ENGINE
+app.set("view engine", "hbs");
 
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 
-//SET STATIC FOLDER for public files
-app.use( express.static(__dirname + '/public') );
+// REGISTER THE PARTIALS FOLDER
+hbs.registerPartials(__dirname + "/views/partials");
 
-//REGISTER THE FOLDER FOR PARTIALS
-hbs.registerPartials(__dirname + '/views/partials')
 
-//ROUTES
-app.get('/users/:username', (req, res, next) => {  //Gets data from the url that is entered by user
-    res.send(req.params)
+//MIDDLEWARE
+// SET THE FOLDER FOR PUBLIC FILES
+app.use(express.static(__dirname + "/public"));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+// ROUTES
+// localhost:3000/users/ross-u/webdev/barcelona
+app.get("/users/:username/webdev/:city", (req, res, next) => {
+  res.send(req.params);
+  // req.params.username = 'ross-u'
+  // req.params.city = 'barcelona'
 });
 
+app.get("/books/:bookId", (req, res) => {
+  res.send(req.params);
+  const bookId = req.params.bookId;
+});
 
-//CREATE A BASIC FUNCTION
+app.get("/search", (req, res) => {
+  res.send(req.query);
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/email", (req, res) => {
+//   console.log("req.path ->", req.path);
+//   console.log("req.query ->", req.query);
+//   console.log("req.params ->", req.params);
+//   console.log("req.method ->", req.method);
+//   console.log("req.headers ->", req.headers);
+
+  res.send(req.query);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
